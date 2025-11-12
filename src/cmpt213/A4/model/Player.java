453bf7ed19a87856f8deb2365b8ebbdded96be23
Anna Markovsky <156268866+anna-markovsky@users.noreply.github.com>
2025-11-12 12:22:41 -0800

    Update Player.java

--- src/cmpt213/A4/model/Player.java
+++ src/cmpt213/A4/model/Player.java
@@ -5,12 +5,12 @@ public class Player {
     private final int DEFAULT_HEALTH = 600;
     private final int MAX_NUM_RINGS = 3;
     private int health;
-    private Rings.Ring[] rings;
+    //private Rings.Ring[] rings;
 
     public Player() {
         this.health = DEFAULT_HEALTH;
-        rings = new Rings.Ring[3];
-        rings[0] = new Rings.NullRing();
+        //rings = new Rings.Ring[3];
+        //rings[0] = new Rings.NullRing();
     }
 
     public void decreaseHealth(int damageReceived){
